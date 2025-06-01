5b024656ac885d292e5103a92ebbe62c504f26ba
deeto15 <ke05739@georgiasouthern.edu>
2025-06-01 17:38:10 -0400

    deprecated old scraper

--- scraper/main.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from scraper.past_scraper import run_past_scraper
-from scraper.live_scraper import run_live_scraper
-
-
-def main():
-    try:
-        asyncio.run(run_live_scraper())
-
